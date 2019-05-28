6a5bb260f84a5dbf0aa941915f77dabf34487c02
muhsink99 <muhsinkumbay99@gmail.com>
2019-05-28 01:36:14 +0100

    setup basic project structure

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils import setup
+setup(
+    name == 'Zebot2',
+    version == '0.1.0',
+    description == 'A rewrite of the original Zebot',
+    author == 'Muhsin Kumbay',
+    author_email == 'muhsinkumbay99@gmail.com'
+)
